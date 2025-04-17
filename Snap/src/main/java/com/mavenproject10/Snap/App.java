c29289c9e7dcee7077b88f5eedc520c6d85900dd
sanjaygk7 <sanjaykamath6969@gmail.com>
2025-04-17 09:28:06 +0530

    first commit

---
+++ Snap/src/main/java/com/mavenproject10/Snap/App.java
@@ -0,0 +1,13 @@
+package com.mavenproject10.Snap;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
